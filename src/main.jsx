3261f608f0116a900c7366219b0224b6f31fb343
codingsapienss <contactprsant@gmail.com>
2023-12-04 23:45:47 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Provider store={store} >
+      <Provider store={store}>
         <App />
       </Provider>
     </Router>
   </React.StrictMode>
-)
+);
